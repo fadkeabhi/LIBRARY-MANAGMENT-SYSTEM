c4b365f0da4add360a90bd4206a337b3087eb062
Palash Dhabale <100768184+PalashDhabale@users.noreply.github.com>
2022-11-20 11:53:49 +0530

    done editing books

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ using namespace std;
 int main()
 {
 	LIB obj;
+	/*obj.add_student();
 	obj.add_student();
-	obj.add_student();
 	obj.show_all_stud();
 	obj.remove_student();
 	obj.show_all_stud();
@@ -20,5 +20,23 @@ int main()
 	obj.show_all_stud();
 	obj.remove_student();
 	obj.show_all_stud();
+	*/
+	cout<<"--------------"<<endl;
+	obj.add_book();
+	cout<<"--------------"<<endl;
+	obj.add_book();
+	cout<<"--------------"<<endl;
+	obj.add_book();
+	cout<<"--------------"<<endl;
+	obj.add_book();
+	cout<<"--------------"<<endl;
+	obj.show_all_books();
+	cout<<"--------------"<<endl;
+	// obj.remove_book();
+	// cout<<"--------------"<<endl;
+	obj.edit_book();
+	obj.show_all_books();
+
+
 	return 0;
 }
